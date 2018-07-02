924942084112071cd4f689cd00faaa23c2245264
Basiji <allvpn.app@gmail.com>
2018-07-02 20:00:45 +0430

    x

--- modules/router.js
+++ modules/router.js
@@ -1,17 +1,90 @@
+
+var unirest = require('unirest');
+var config = require('./config');
+
 function router(req, res, connection){
     
     // Get user ID
     var userid = req.body.message.from.id;
 
-    // Log message
-    console.log(req.body.message);
-
-    // Get message type
+    // Get message 
     var message = parseMessage(req.body.message);    
+    
+    // * Log Message
     console.log(message);
-    res.sendStatus(200);
+    
+    // Check for commands
+    if(message.type === 'bot_command')
+        handleCommands(message);
+    
+    // Check for user inputs
+    else 
+        handleInputs(message);
+}
+
+
+function handleCommands(message){
+    switch(message.text){
+        case '/start':
+        // Register user info (if not exists)
+        registerUser(message);
+
+        case '/test':
+        // Test message
+        break;
+        default:
+        return;
+    }
+}
+
+function registerUser(message){
+    // Check for userID
+    connection.query("SELECT * FROM bot_suers WHERE userid = '" + message.from.id + "'", function (error, result){
+
+        if(error)
+        return;
+
+        // Register new user
+        if(result.length === 0){
+
+            connection.query("INSERT INTO bot_users SET ?",
+            {
+                userid:message.from.id,
+                name:message.form.first_name,
+            },
+            function(error){
+                
+                if(error)
+                return console.log(error);
+                
+                // Welcome message
+                sendMessage(message.chat.id, 'Registered ' + message.from.first_name);
+
+
+            });
+
+        } else {
+
+            // Welcome message
+            return sendMessage(message.chat.id, 'Logined ' + message.from.first_name);
+
+        }
+
+
+    });
+}
+
+
+function sendMessage(chat_id, text){
+    unirest
+    .post(config.BOTURL + 'sendMessage')
+    .send({'chat_id':chat_id,'text':text})
+    .end(function(response){
+    console.log(response);
+});
 
 }
+
 
 function parseMessage(message){
     // Command message
