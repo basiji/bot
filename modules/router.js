c5ff2358ec1df68135662aa3bca2d7b3b3dcc607
Basiji <allvpn.app@gmail.com>
2018-07-02 19:32:46 +0430

    x

--- modules/router.js
+++ modules/router.js
@@ -7,11 +7,27 @@ function router(req, res, connection){
     console.log(req.body.message);
 
     // Get message type
-    var type = req.body.message.entities[0].type || 'null';
+    var message = parseMessage(req.body.message);    
+    console.log(message);
 
     console.log(type);
     res.sendStatus(200);
 
 }
+
+function parseMessage(message){
+    // Command message
+    if(message.entities)
+        return {
+            type:'command',
+            text:message.entities[0].text
+        };
+    // User input message
+    else return {
+        type:'text',
+        text:message.text
+    }
+}
+
 
 module.exports = router;
