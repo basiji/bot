b0ae359cfc64e74e44681e8423751bb87e961e7b
Basiji <allvpn.app@gmail.com>
2018-07-02 19:27:09 +0430

    x

--- modules/router.js
+++ modules/router.js
@@ -3,8 +3,11 @@ function router(req, res, connection){
     // Get user ID
     var userid = req.body.message.from.id;
 
+    // Log message
+    console.log(req.body.message);
+
     // Get message type
-    var type = req.body.message.entities[0].type;
+    var type = req.body.message.entities[0].type || 'null';
 
     console.log(type);
     res.sendStatus(200);
