ad2d754b147fc89a281b5247f5abd486d011f52a
Basiji <allvpn.app@gmail.com>
2018-07-02 17:35:26 +0430

    Change Callback port to 80

--- devcodes/set_webhook.js
+++ devcodes/set_webhook.js
@@ -3,7 +3,7 @@ var config = require(__dirname + '/config.js');
 
 // Making a Post Request
 unirest.post(config.BOTURL + 'setWebHook')
-.send({'url':'https://78.46.119.98:3000'})
+.send({'url':'https://78.46.119.98:80'})
 .end(function(response){
     console.log(response);
 });
