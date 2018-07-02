5f69801adc1cd596a575e06243e2fdea1e4850a5
Basiji <allvpn.app@gmail.com>
2018-07-02 17:34:38 +0430

    x

--- devcodes/set_webhook.js
+++ devcodes/set_webhook.js
@@ -2,7 +2,7 @@ var unirest = require('unirest');
 var config = require(__dirname + '/config.js');
 
 // Making a Post Request
-unirest.post(config.BOTURL + '/setWebHook')
+unirest.post(config.BOTURL + 'setWebHook')
 .send({'url':'https://78.46.119.98:3000'})
 .end(function(response){
     console.log(response);
