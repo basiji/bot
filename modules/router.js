5bfa9b47a45f7685a57f91989ebbef643202ecc6
Basiji <allvpn.app@gmail.com>
2018-07-03 11:23:34 +0430

    x

--- modules/router.js
+++ modules/router.js
@@ -68,7 +68,7 @@ function handleMessage(message, connection){
                     inline_keyboard:[
                         [{
                             text:'ورود به درگاه پرداخت',
-                            url:'bit.ly/2NkEkN2?token=' + token
+                            url:'https://Zarinp.al/188166' + token
                         }]]
                     }));
     
