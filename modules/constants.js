7fd420a80aeada62ec751e67f3541831dffec465
Basiji <allvpn.app@gmail.com>
2018-07-03 18:35:40 +0430

    x

--- modules/constants.js
+++ modules/constants.js
@@ -2,7 +2,7 @@ var emoji = require('node-emoji');
 
 module.exports = {
     SERVER_ERROR:'متاسفانه خطایی رخ داده است. لطفا لحظاتی بعد مجددا امتحان کنید.',
-    BUY_VOUCHER: emoji.get('white_check_mark') + ' خرید ووچر پرفکت مانی',
+    BUY_VOUCHER: 'خرید ووچر پرفکت مانی',
     ORDER_HISTORY:'سوابق خرید',
     SUPPORT: 'پشتیبانی',
     CUSTOM_VOUCHER:'مبلغ دلخواه (تا سقف 500 دلار)',
@@ -10,12 +10,13 @@ module.exports = {
     SELECT_VOUCHER:'لطفا مبلغ مورد نظر خود را انتخاب نمایید.',
     MAIN_MENU:'منوی اصلی',
     CALL_TO_ACTION:'لطفا یک گزینه را انتخاب نمایید.',
+    FOO:'12345678987654321',
     WELCOME_MESSAGE:'به سامانه خودکار فروش پرفکت مانی خوش آمدید. جهت ادامه کار لطفا یکی از گزینه ها را انتخاب نمایید.',
     INVOICE:"<b>Invoice</b> <a href='tg://#123'> #123 </a> \n<pre>Amount: %usdprice% USD \n" +
     "Exchange rate: 56,000 \nIRR: %irrprice% </pre>\n ---- \n<b>Information</b>\n<pre>Voucher code:" +
-    " %vouchercode% \nActivation code: xxxxxxxxxx </pre> " +
+    " %vouchercode% \nActivation code: xxxxxxxxxxxxxxxxx </pre> " +
     "\n \n کد فعالسازی پس از انجام تراکنش بصورت اتوماتیک ارسال خواهد شد.",
     NO_ORDER_MESSAGE:'شما تا بحال سفارشی نداشته اید.',
-    ORDER_TEMPLATE:"<b>Invoice #%id% </b>\n <pre>Date: %date% \nAmount: %price% IRR\nStatus: %status%</pre>\n----\n",
+    ORDER_TEMPLATE:"<b>Invoice #%id% </b>\n <pre>Date: %date% \nAmount: %price% IRR \nStatus: %status%\ne-Voucher: %voucher% \nActivation code: %activation%</pre>\n----\n",
     SUPPORT_MESSAGE:"جهت ارتباط با بخش پشتیبانی و پیگیری مشکلات لطفا با آیدی @pmvoucher_admin در ارتباط باشید."
 }
--- modules/router.js
+++ modules/router.js
@@ -51,101 +51,94 @@ function handleMessage(message, connection){
         break;
 
         default:
-            if(message.text.includes('$')){
-                // Voucher Selected -> create transaction record
-                var usdprice = message.text.split(' ')[0];
-                var irrprice = usdprice * config.USDPRICE * 10;
-
-                connection.query("INSERT INTO bot_transactions SET ?",
-                {
-                    userid:message.from.id,
-                    price:irrprice
-                }, function(error, result){
-                    
-                    if(error)
-                    //return sendMessage(message.chat.id, constants.SERVER_ERROR, keyboard.voucher_menu);
-                    console.log(error);
-
-                    // Generate encrypted token
-                    var payment_id = result.insertId;
-                    var userid = message.from.id;
-                    var token = CryptoJS.AES.encrypt(payment_id + "#" + userid, config.SECRET_KEY);
-                    
-
-                    var response = constants.INVOICE
-                    .replace('%usdprice%',usdprice)
-                    .replace('%irrprice%',irrprice.toLocaleString())
-                    .replace('%vouchercode%',Math.floor((Math.random() * 9999999999) + 1));
-
-
-                    // Prepare payment gateway
-                    var options = {
-                    method: 'POST',
-                    uri: config.ZARINPAL_REQUEST,
-                    headers: {
-                    'cache-control': 'no-cache',
-                    'content-type': 'application/json'
-                    },
-                    body: {
-                    MerchantID : config.ZARINPALTOKEN,
-                    Amount : irrprice / 10,
-                    Description : 'Some description',
-                    CallbackURL : 'http://google.com'
-                    },
-                    json: true 
-                    };
-
-                    request(options)
-                    .then(function (data) {
-                    var Status = data.status;
-                    
-                    // If requets failed
-                    if(Status !== 200)
-                    //return sendMessage(message.chat.id, constants.SERVER_ERROR, keyboard.voucher_menu);
-                    console.log(data);
-
-                    var Authority = data.Authority;
-                    // Send Message with payment button
-                    sendMessage(
-                        message.chat.id,
-                        response,
-                        JSON.stringify({
-                        inline_keyboard:[
-                            [{
-                                text:'ورود به درگاه پرداخت',
-                                url: config.ZARINPAL_GATEWAY + Authority + '/' + config.BANKS.ASAN
-                            }]]
-                        }));
-                    })
-                    
-                    // if request failed
-                    .catch(function (err) {
-                        return sendMessage(message.chat.id, constants.SERVER_ERROR, keyboard.voucher_menu);
-                        console.log(err);
-                    });
-                });
-    
-            } else if (!isNaN(message.text)){
-                // Custom price
-                var price = message.text;
-                if(parseInt(price) %5 !== 0)
-                return sendMessage(message.chat.id, 'Invalid input.');
-                return sendMessage(message.chat.id, 'You entered : ' + price + 'USD');
-            } else if (message.text.includes('@')) {
-                // Email verification
-                var email = message.text;
-                if(validator.validate(email)){
-                    updateUserEmail(message, email, connection);
-                } else {
-                    sendMessage(message.chat.id, 'Email incorrect.');
-                }
-            } else {
-                return;
-            }
-        
+            if(message.text.includes('$'))
+            prepareInvoice(message, connection);
+            else 
+            return;
     }
 }
 
+// Prepare voucher invoice
+function prepareInvoice(message, connection){
+       // Voucher Selected -> create transaction record
+       var usdprice = message.text.split(' ')[0];
+       var irrprice = usdprice * config.USDPRICE * 10;
+       var vouchercode = Math.floor((Math.random() * 9999999999) + 1);
+       var activationcode = constants.FOO.split('').sort(function(){return 0.5-Math.random()}).join('');
+       connection.query("INSERT INTO bot_transactions SET ?",
+       {
+           userid:message.from.id,
+           price:irrprice,
+           vouchercode:vouchercode,
+           activationcode:activationcode
+
+       }, function(error, result){
+           
+           if(error)
+           //return sendMessage(message.chat.id, constants.SERVER_ERROR, keyboard.voucher_menu);
+           console.log(error);
+
+           // Generate encrypted token
+           var payment_id = result.insertId;
+           var userid = message.from.id;
+           var token = CryptoJS.AES.encrypt(payment_id + "#" + userid, config.SECRET_KEY);
+           
+
+           var response = constants.INVOICE
+           .replace('%usdprice%',usdprice)
+           .replace('%irrprice%',irrprice.toLocaleString())
+           .replace('%vouchercode%',vouchercode);
+
+
+           // Prepare payment gateway
+           var options = {
+           method: 'POST',
+           uri: config.ZARINPAL_REQUEST,
+           headers: {
+           'cache-control': 'no-cache',
+           'content-type': 'application/json'
+           },
+           body: {
+           MerchantID : config.ZARINPALTOKEN,
+           Amount : irrprice / 10,
+           Description : 'Some description',
+           CallbackURL : 'http://google.com'
+           },
+           json: true 
+           };
+
+           request(options)
+           .then(function (data) {
+           var Status = data.status;
+           
+           // If requets failed
+           if(Status !== 200)
+           //return sendMessage(message.chat.id, constants.SERVER_ERROR, keyboard.voucher_menu);
+           console.log(data);
+
+           var Authority = data.Authority;
+           // Send Message with payment button
+           sendMessage(
+               message.chat.id,
+               response,
+               JSON.stringify({
+               inline_keyboard:[
+                   [{
+                       text:'ورود به درگاه پرداخت',
+                       url: config.ZARINPAL_GATEWAY + Authority + '/' + config.BANKS.ASAN
+                   }]]
+               }));
+           })
+           
+           // if request failed
+           .catch(function (err) {
+               return sendMessage(message.chat.id, constants.SERVER_ERROR, keyboard.voucher_menu);
+               console.log(err);
+           });
+       });
+}
+
+// Update user email address
 function updateUserEmail(message, email, connection){
     connection.query("UPDATE bot_users WHERE userid = '" + message.from.id + "' SET email = '" + email + "'",function(error){
         if(error)
@@ -156,6 +149,7 @@ function updateUserEmail(message, email, connection){
     });
 }
 
+// Register new user (if not exists)
 function registerUser(message, connection){
     
     // Check for userID
@@ -191,6 +185,7 @@ function registerUser(message, connection){
     });
 }
 
+// Get orders history
 function getOrders(message, connection){
     connection.query("SELECT * FROM bot_transactions WHERE userid = '" + message.from.id + "' ORDER BY ID DESC",function(error, result){
 
@@ -205,8 +200,11 @@ function getOrders(message, connection){
 
             response += constants.ORDER_TEMPLATE
                         .replace('%id%',item.id)
+                        .replace('%price%',item.price)
+                        .replace('%voucher%',item.vouchercode)
+                        .replace('%activation',item.activationcode)
                         .replace('%date','2018-02-07')
-                        .replace('%status%','Success');
+                        .replace('%status%',item.activationcode.includes('xxx') ? 'Failed' : 'Success');
 
         });
 
@@ -215,6 +213,7 @@ function getOrders(message, connection){
     });
 }
 
+// General send message function
 function sendMessage(chat_id, text, keyboard = []){
     unirest
     .post(config.BOTURL + 'sendMessage')
