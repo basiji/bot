dc8a828bf3e486c132e5dba8c638ff5780a5753d
Basiji <allvpn.app@gmail.com>
2018-07-03 17:22:10 +0430

    x

--- modules/constants.js
+++ modules/constants.js
@@ -9,7 +9,8 @@ module.exports = {
     EMAIL_UPDATE_SUCCESS:'آدرس ایمیل با موفقیت ثبت شد.',
     SELECT_VOUCHER:'لطفا مبلغ مورد نظر خود را انتخاب نمایید.',
     WELCOME_MESSAGE:'به سامانه خودکار فروش پرفکت مانی خوش آمدید. جهت ادامه کار لطفا یکی از گزینه ها را انتخاب نمایید.',
-    INVOICE:"<b>Invoice</b> \n Amount : %usdprice% \n" +
-    " Exchange rate : 56,000 \n  IRR : %irrprice% \n ---- \n  Voucher Code : %vouchercode% " + 
-    " Activation code : 1234-56789-xxxx \n کد فعالسازی پس از انجام تراکنش بصورت اتوماتیک بروزرسانی میشود."
+    INVOICE:"<b>Invoice</b> \n<pre>Amount:  %usdprice% \n" +
+    "Exchange rate:  56,000 \nIRR:  %irrprice% </pre>\n ---- \n<b> Voucher info </b>\nVoucher Code:" +
+    "<code style='color:blue'>  %vouchercode% </code>\nActivation code:  <code>1234-56789-xxxx</code>" +
+    "\n \n کد فعالسازی پس از انجام تراکنش بصورت اتوماتیک بروزرسانی میشود."
 }
